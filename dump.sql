50310d2fe3b0b3e5471672b4e7f8ba9881d8b078
Erick <165854538+Erick-3698@users.noreply.github.com>
2024-06-18 02:00:25 -0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,49 +1,89 @@
-DROP DATABASE IF EXISTS db_task_explorer;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18/06/2024 às 05:23
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+DROP DATABASE IF EXISTS db_task_explorer;
 CREATE DATABASE db_task_explorer;
 USE db_task_explorer;
 
-CREATE TABLE `usuarios` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(200) NOT NULL,
-  `senha` VARCHAR(100) NOT NULL,
-  `nome` VARCHAR(100) NOT NULL,
-  `tipo_usuario` ENUM('Usuário', 'Administrador') NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-);
-
 CREATE TABLE `tasks` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `task_name` VARCHAR(190),
-  `task_description` VARCHAR(250),
-  `task_image` VARCHAR(50),
-  `task_date` DATE,
-  `id_usuario` INT,
-  FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
-);
+  `id` int(11) NOT NULL,
+  `task_name` varchar(190) DEFAULT NULL,
+  `task_description` varchar(250) DEFAULT NULL,
+  `task_image` varchar(50) DEFAULT NULL,
+  `task_date` date DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 
 CREATE TABLE `tbl_list` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` VARCHAR(45),
-    `list` VARCHAR(255) NOT NULL,
-    `painel` INT,
-    `id_task` INT,
-    FOREIGN KEY (`id_task`) REFERENCES `tasks` (`id`)
-  );
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `list` varchar(255) NOT NULL,
+  `painel` int(11) DEFAULT NULL,
+  `id_task` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-CREATE TABLE `cartoes` (
-  `id_cartao` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `titulo` VARCHAR(45) NOT NULL,
-  `descricao` VARCHAR(500),
-  `nivel_prioridade` ENUM('ALTA', 'MÉDIA', 'BAIXA'),
-  `id_lista` INT NOT NULL,
-  FOREIGN KEY (`id_lista`) REFERENCES `tbl_list` (`id`)
-);
 
-INSERT INTO `usuarios` (id_usuario, email, senha, nome, tipo_usuario) VALUES (1, 'admin@email.com', '123456', 'Admin', 'Administrador');
-INSERT INTO `usuarios` (id_usuario, email, senha, nome, tipo_usuario) VALUES (2, 'user01@email.com', '123456', 'User01', 'Usuário');
-INSERT INTO `usuarios` (id_usuario, email, senha, nome, tipo_usuario) VALUES (3, 'user02@email.com', '123456', 'User02', 'Usuário');
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `tipo_usuario` enum('usuário','Administrador') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-INSERT INTO `tasks` (id, task_name, task_description, task_image, task_date) VALUES (1, 'Engenharia', 'Quadro Kanban sobre Engenharia', 'mohammad-alizade-62t_kKa2YlA-unsplash.jpg', '');
-INSERT INTO `tasks` (id, task_name, task_description, task_image, task_date) VALUES (2, 'Desenvolvimento de Software', 'Quadro Kanban sobre o desenvolvimento de um software de inteligência artificial (IA)', 'christian-lue-QcJ1XCc3gJo-unsplash.jpg', '');
-INSERT INTO `tasks` (id, task_name, task_description, task_image, task_date) VALUES (3, 'Medicina', 'Quadro Kanban com dados levantados do número de médicos contratados', 'bailey-zindel-NRQV-hBF10M-unsplash.jpg', '');
+
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_tasks_UNIQUE` (`id`),
+  ADD KEY `id_usuario_tasks_idx` (`id_usuario`);
+
+ALTER TABLE `tbl_list`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_tarefa_UNIQUE` (`id`),
+  ADD KEY `id_tasks_list_idx` (`id_task`);
+
+
+
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `id_usuario_UNIQUE` (`id_usuario`);
+
+
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `tbl_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `id_usuario_tasks` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `tbl_list`
+  ADD CONSTRAINT `id_tasks_list` FOREIGN KEY (`id_task`) REFERENCES `tasks` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
